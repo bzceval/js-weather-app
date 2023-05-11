9f59de569b85483dd672ae99c8653087cb6b6aaa
busra <busraaceval@gmail.com>
2023-05-11 10:23:49 +0300

    add encryted api key

--- weather.js
+++ weather.js
@@ -10,7 +10,24 @@ const list = document.querySelector(".ajax-section .cities");
 // (reactta => .env ya da config içerisinde tutulur)
 // (javascriptte=> aes256 şifreleme algoritması var ve kullanımı Extentions.js içerisindeli ilk function gelen string ifade encrypt olurken diğer function da ise gelen string ifade decrypt olur )
 // böylelikle bu key şifreleme algoritması ile tutulur. Ve bu token consolede anlamsız characterler ile görünür.
+// bir kereye mahsus yapıp storageden anlamsız charları almalısın ve tekrar api key olarak set etmelisin.
+// localStorage.setItem(
+//   "tokenKeyEncryted",
+//   EncryptStringAES("b99c3dd733d7b532e423e732f33abf88")
+// );
+
+// şifrelenmiş token key localstorageye kayıt olunur.
 localStorage.setItem(
-  "tokenKeyEncryted",
-  EncryptStringAES("b99c3dd733d7b532e423e732f33abf88")
+  "tokenKey",
+  "TedQEZxvfj0DDPYuSmX389ROEaTvFzh4u/U4C7AVoo30zk63m9v0V4+NqgWLvQIt"
 );
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // event nerede kullanılıyorsa orada eventi prevent yapmalısın.
+  getWeatherDataFromApi();
+});
+
+// Get Api Func (http methods = verbs)
+const getWeatherDataFromApi = () => {
+  // const tokenKey =
+};
